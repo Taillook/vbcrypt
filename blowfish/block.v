bb6bfeed899a9a01fec0be59ac12200aa6d72c8d
Taillook <jousoku@gmail.com>
2021-09-05 00:59:38 +0900

    fix: fmt

--- blowfish/block.v
+++ blowfish/block.v
@@ -42,7 +42,7 @@ pub fn expand_key(key []byte, mut bf Blowfish) {
 pub fn expand_key_with_salt(key []byte, salt []byte, mut bf Blowfish) {
 	mut j := 0
 	for i := 0; i < 18; i++ {
-		bf.p[i] ^= get_next_word(key, mut &j)
+		bf.p[i] ^= get_next_word(key, &j)
 	}
 
 	j = 0
@@ -50,33 +50,33 @@ pub fn expand_key_with_salt(key []byte, salt []byte, mut bf Blowfish) {
 	mut l := u32(0)
 	mut r := u32(0)
 	for i := 0; i < 18; i += 2 {
-		l ^= get_next_word(key, mut &j)
-		r ^= get_next_word(key, mut &j)
+		l ^= get_next_word(key, &j)
+		r ^= get_next_word(key, &j)
 		arr := encrypt_block(l, r, mut bf)
 		bf.p[i], bf.p[i + 1] = arr[0], arr[1]
 	}
 
 	for i := 0; i < 256; i += 2 {
-		l ^= get_next_word(key, mut &j)
-		r ^= get_next_word(key, mut &j)
+		l ^= get_next_word(key, &j)
+		r ^= get_next_word(key, &j)
 		arr := encrypt_block(l, r, mut bf)
 		bf.s[0][i], bf.s[0][i + 1] = arr[0], arr[1]
 	}
 	for i := 0; i < 256; i += 2 {
-		l ^= get_next_word(key, mut &j)
-		r ^= get_next_word(key, mut &j)
+		l ^= get_next_word(key, &j)
+		r ^= get_next_word(key, &j)
 		arr := encrypt_block(l, r, mut bf)
 		bf.s[1][i], bf.s[1][i + 1] = arr[0], arr[1]
 	}
 	for i := 0; i < 256; i += 2 {
-		l ^= get_next_word(key, mut &j)
-		r ^= get_next_word(key, mut &j)
+		l ^= get_next_word(key, &j)
+		r ^= get_next_word(key, &j)
 		arr := encrypt_block(l, r, mut bf)
 		bf.s[2][i], bf.s[2][i + 1] = arr[0], arr[1]
 	}
 	for i := 0; i < 256; i += 2 {
-		l ^= get_next_word(key, mut &j)
-		r ^= get_next_word(key, mut &j)
+		l ^= get_next_word(key, &j)
+		r ^= get_next_word(key, &j)
 		arr := encrypt_block(l, r, mut bf)
 		bf.s[3][i], bf.s[3][i + 1] = arr[0], arr[1]
 	}
@@ -122,9 +122,12 @@ fn encrypt_block(l u32, r u32, mut bf Blowfish) []u32 {
 	return [xl, xr]
 }
 
-fn get_next_word(b []byte, mut pos &int) u32 {
+fn get_next_word(b []byte, pos &int) u32 {
 	mut w := u32(0)
-	mut j := pos
+	mut j := 0
+	unsafe {
+		j = pos
+	}
 	for i := 0; i < 4; i++ {
 		w = w << 8 | u32(b[j])
 		j++
