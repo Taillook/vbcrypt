7bba89324953696c253d5d33f02789f473dcf03e
Taillook <jousoku@gmail.com>
2021-09-11 09:59:42 +0900

    fix: example

--- example/example.v
+++ example/example.v
@@ -1,17 +1,22 @@
 module main
 
-import example.vbcrypt
+import taillook.vbcrypt
 
 fn main() {
 	hash := vbcrypt.generate_from_password('password'.bytes(), 10) or {
 		println(err)
 		return
 	}
-	println(hash)
+	println("hash: $hash")
 
 	vbcrypt.compare_hash_and_password('password'.bytes(), hash.bytes()) or {
 		println(err)
 		return
 	}
 	println('matched password and hash')
+
+	vbcrypt.compare_hash_and_password('password2'.bytes(), hash.bytes()) or {
+		println(err)
+		return
+	}
 }
